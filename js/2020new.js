7264b2466f0fc743444ad2f9a6c7e5cbf3e0b601
Dmitry <dmitriy.marvis@gmail.com>
2020-07-01 07:51:54 +0300

    First task complit

--- js/2020new.js
+++ js/2020new.js
@@ -27,4 +27,26 @@
 // alert(`Привет, ${user}`);
 
 
-console.log(10%6);
+// console.log(10%6);
+
+
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {}, // в это свойство поместить пустой объект
+    actors: {}, // тоже поместить пустой объект
+    genres: [], // сюда поместить пустой массив
+    privat: false // - в это свойство поместить boolean(логическое) значение false
+};
+    
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('На сколько оцените его?', ''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
